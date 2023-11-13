bd867f9b9427ff0955774c93940bd31a0ad1b9c6
Dev.SoNB <wjdgh2777@naver.com>
2023-11-13 12:43:33 +0900

    Init: Spring Boot 초기구성

---
+++ src/main/java/SpringGroup/SpringResult/SpringResultApplication.java
@@ -0,0 +1,13 @@
+package SpringGroup.SpringResult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringGroup/SpringResult/SpringResultApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringGroup.SpringResult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
