1a756b8723f39c24d2eb4ab318d2d11c9658d10c
Dev.SoNB <wjdgh2777@naver.com>
2023-11-15 13:05:50 +0900

    Feat: 구조 변경 & member 레포지토리 생성

--- src/main/java/SpringGroup/SpringResult/init/controller/helloController.java
+++ src/main/java/SpringGroup/SpringResult/init/controller/helloController.java
@@ -1,4 +1,4 @@
-package SpringGroup.SpringResult.domain.init.controller;
+package SpringGroup.SpringResult.init.controller;
 
 // Annotation 사용을 위해
 import org.springframework.stereotype.Controller;
---
+++ src/main/java/SpringGroup/SpringResult/repository/memoryMemberRepository.java
@@ -0,0 +1,41 @@
+package SpringGroup.SpringResult.repository;
+
+import SpringGroup.SpringResult.domain.member;
+
+import java.util.*;
+
+// memoryMemberRepository가 memberRepository(인터페이스)를 구현
+public class memoryMemberRepository implements memberRepository {
+
+  // Map: java의 데이터 저장 방식 중 하나
+  // key: Long, value: Member
+  private static Map<Long, member> store = new HashMap<>();
+
+  // sequence: key값을 생성해줌
+  private static long sequence = 0L;
+
+  @Override
+  public member save(member member) { // save
+    member.setId(++sequence); // sequence값 증가(key값)
+    store.put(member.getId(), member); // store에 저장
+    return member;
+  }
+
+  @Override
+  public Optional<member> findById(Long id) { // findById
+    // null일 수 있어 Optional.ofNullable() 로 감쌈
+    return Optional.ofNullable(store.get(id)); // id값으로 해당 객체 store에서 꺼내서 return
+  }
+
+  @Override
+  public Optional<member> findByName(String name) { // findByName
+    return store.values().stream() // value값들을 루프를 돌아서
+        .filter(member -> member.getName().equals(name)) // param로 넘어온 name과 같은 것을 filtering
+        .findAny(); // 처음 filtering 된 요소 1개를 return
+  }
+
+  @Override
+  public List<member> findAll() { // findAll
+    return new ArrayList<>(store.values()); // 저장소에 있는 value들을 List로 반환
+  }
+}
